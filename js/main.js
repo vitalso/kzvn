7e16d129407497883524dfe7814b4fa31efda63f
Vitalii <lgphone.vs@gmail.com>
2025-08-04 15:56:25 +0300

    fix font-weight and update toggle modal

--- js/main.js
+++ js/main.js
@@ -9,13 +9,23 @@ if ( button ) {
 }
 
 // Close modal
+const openButton = document.getElementsByClassName('openModal');
 const closeButton = document.getElementsByClassName('close');
 const modal = document.getElementById('modal');
 
 if ( closeButton.length > 0 ) {
   Array.from(closeButton).forEach(button => {
     button.addEventListener('click', function() {
-      modal.classList.toggle('hidden');
+      modal.classList.toggle('!block');
+    });
+  });
+}
+
+if ( openButton.length > 0 ) {
+  Array.from(openButton).forEach(button => {
+    button.addEventListener('click', function(e) {
+      e.preventDefault();
+      modal.classList.toggle('!block');
     });
   });
 }
